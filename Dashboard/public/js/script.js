67029b5ea23c5d37c587feea80f6f03992c303de
profwyattb <profwyattb@gmail.com>
2020-08-31 15:33:10 +0100

    Rewrite without jquery + tidy

--- Dashboard/public/js/script.js
+++ Dashboard/public/js/script.js
@@ -1,136 +1,237 @@
-// const e = require("express");
+let info = window.info;
 
-let info;
+//Helper code
+const $ = document.querySelectorAll.bind(document);
+const $_ = document.querySelector.bind(document);
+const isEmpty = (value) => {
+  if (value == null || value.length == 0) return true;
+  else return false;
+};
 
-$(document).ready(() => {
-  feather.replace();
-  particlesJS.load("particles", "js/particles.json");
-  info = window.info;
-  $(`input[value=${info.iface}]`).attr("checked", true);
-  welcome(info.name);
-  mounts();
-});
-
+//Functions
 const welcome = (name) => {
   const hour = new Date().getHours();
   let period;
   if (hour >= 4 && hour <= 11) period = "morning";
   if (hour >= 12 && hour <= 16) period = "afternoon";
   if (hour >= 17) period = "evening";
-  $("#welcome").html(`Good ${period}, ${name}`);
+  $_("#welcome").innerHTML = `Good ${period}, ${name}`;
 };
-
-//Change pages
-const settings = (value) => {
-  if (value == "back") {
-    $("#page1").css("display", "flex");
-    $("#page2").css("display", "none");
-    $("#settings-form").trigger("reset");
-  } else {
-    $("#page1").css("display", "none");
-    $("#page2").css("display", "flex");
+const mounts = (devices) => {
+  if (!devices) {
+    return;
   }
-};
-
-const devices = (value) => {
-  if (value == "back") {
-    $("#page2").css("display", "flex");
-    $("#page3").css("display", "none");
-  } else {
-    $("#page2").css("display", "none");
-    $("#page3").css("display", "flex");
+  for (let i = 0; i < devices.length; i++) {
+    $_("#noDevices").style.display = "none";
+    $(".mount")[i].style.display = "flex";
+    $(".mount p")[i].innerHTML = devices[i].mount_point.replace("/", "");
   }
-};
-
-const mounts = () => {
-  $(".mount").each((index, obj) => {
-    if (index >= window.devices.length) {
-      return;
-    }
-    let mountName = window.devices[index].mount_point.replace("/", "");
-    $(obj).css("display", "flex");
-    $("#noDevices").css("display", "none");
-    $("p", obj).text(mountName);
-  });
   return;
 };
-//Terminal link
-const terminal = () => {
-  window.alert("⚠ Please note: This tool is very powerful and should only be used if you know what you're doing!");
-  window.location.href = window.location.origin + ":4200";
+const interfaces = (iface) => {
+  if (iface) {
+    $_(`input[value=${info.iface}]`).attr("checked", true);
+  } else {
+    $_("#interface-message").innerHTML = "Interface not detected! <span id='iface-more-info'>More info</span>";
+    $_("#iface-more-info").addEventListener("click", () => {
+      window.alert(
+        "This alert means that the web interface couldn't work out what interface you're using. If this error continues after a reboot, please file an issue.",
+      );
+    });
+  }
+};
+const pageToggle = (action, page) => {
+  let parentpage;
+  if (page == "settings") parentpage = "home";
+  else if (page == "devices") parentpage = "settings";
+  if (action == "back") {
+    $_(`#${parentpage}-page`).style.display = "flex";
+    $_(`#${page}-page`).style.display = "none";
+    $_("#settings-form").reset();
+  } else {
+    $_(`#${parentpage}-page`).style.display = "none";
+    $_(`#${page}-page`).style.display = "flex";
+  }
 };
 
-//Change adapter
-const adapter = () => {
+//Value setting
+welcome(info.name);
+mounts(info.devices);
+interfaces(info.iface);
+
+//Asset loading
+feather.replace();
+particlesJS.load("particles", "js/particles.json");
+
+//Color change
+const colorchange = (icon) => {
+  colorlist = {
+    "corner-right-up": "#80e27e",
+    "folder": "#6ec6ff",
+    "wifi": "#ff7961",
+    "settings": "#c79100",
+    "submit": "#c79100",
+    "adapter": "#9162e4",
+    "terminal": "#ffc947",
+    "reboot": "#80e27e",
+    "shutdown": "#ff844c",
+    "devices": "#6ec6ff",
+    "umount": "#ff844c",
+    "reset-color": "#000",
+  };
+  color = colorlist[icon];
+  window.pJSDom[0].pJS.particles.array.forEach((item) => {
+    item.color.value = color;
+    item.color.rgb = hexToRgb(color);
+  });
+  window.pJSDom[0].pJS.particles.line_linked.color_rgb_line = hexToRgb(color);
+};
+$(".icon").forEach((item) => {
+  item.addEventListener("mouseenter", () => {
+    colorchange(item.id);
+  });
+  item.addEventListener("mouseleave", () => {
+    colorchange("reset-color");
+  });
+});
+
+//Change pages
+$_("#settings").addEventListener("click", () => {
+  pageToggle("", "settings");
+});
+$_("#back-settings").addEventListener("click", () => {
+  pageToggle("back", "settings");
+});
+$_("#devices").addEventListener("click", () => {
+  pageToggle("", "devices");
+});
+$_("#back-devices").addEventListener("click", () => {
+  pageToggle("back", "devices");
+});
+
+//Settings buttons
+$_("#adapter").addEventListener("click", () => {
   window.alert(
     "⚠ This device will now shutdown. Once the device is shutdown, change adapters and turn the plug on and off. The driver will then be installed and the device will reboot. Then, if the adapter is supported, everything should be working!",
   );
-  $.post("adapter/");
-};
-
-const reboot = () => {
+  const request = new XMLHttpRequest();
+  request.open("get", "adapter/");
+  request.send();
+  setTimeout(() => {
+    location.reload();
+  }, 2000);
+});
+$_("#terminal").addEventListener("click", () => {
+  window.alert("⚠ Please note: This tool is very powerful and should only be used if you know what you're doing!");
+  window.location.href = `${window.location.origin}:4200`;
+});
+$_("#reboot").addEventListener("click", () => {
   window.alert("⚠ This device will now reboot. Continue?");
-  $.post("reboot/");
-};
-
-const shutdown = () => {
+  const request = new XMLHttpRequest();
+  request.open("get", "reboot/");
+  request.send();
+});
+$_("#shutdown").addEventListener("click", () => {
   window.alert("⚠ This device will now shutdown. Continue?");
-  $.post("shutdown/");
-};
+  const request = new XMLHttpRequest();
+  request.open("get", "shutdown/");
+  request.send();
+});
 
+// Eject request
 const eject = (num) => {
-  $(`#eject_${num}`).text("Ejecting");
-  $.ajax({
-    url: "eject/",
-    type: "POST",
-    data: { index: num.toString() },
-    success: () => {
-      $(`#eject_${num}`).text("Save to remove");
-      $(`#eject_${num}`).css("color", "white");
-      $(`#eject_${num}`).css("background-color", "#087f23");
-      setTimeout(() => {
-        $(`#eject_${num}`).parent().css("display", "none");
-      }, 2000);
-      return;
-    },
-    error: () => {
-      $(`#eject_${num}`).text("Error");
-      $(`#eject_${num}`).css("color", "white");
-      $(`#eject_${num}`).css("background-color", "#ba000d");
-      setTimeout(() => {
-        $(`#eject_${num}`).css("color", "");
-        $(`#eject_${num}`).css("background-color", "");
-        $(`#eject_${num}`).text("Eject ⏏");
-      }, 2000);
-    },
-  });
+  $_(`#eject_${num}`).innerHTML = "Ejecting";
+  const request = new XMLHttpRequest();
+  request.open("post", "eject/");
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send(JSON.stringify({ index: num.toString() }));
+  request.onreadystatechange = () => {
+    if (request.readyState == 4) {
+      if (request.status == 200) ejectSuccess(num);
+      else ejectError(num);
+    }
+  };
+};
+const ejectSuccess = (num) => {
+  $_(`#eject_${num}`).innerHTML = "Save to remove";
+  $_(`#eject_${num}`).style.color = "white";
+  $_(`#eject_${num}`).style.backgroundColor = "#087f23";
+  setTimeout(() => {
+    $_(`#eject_${num}`).parentElement.style.display = "none";
+  }, 2000);
+  return;
+};
+const ejectError = (num) => {
+  $_(`#eject_${num}`).innerHTML = "Error";
+  $_(`#eject_${num}`).style.color = "white";
+  $_(`#eject_${num}`).style.backgroundColor = "#ba000d";
+  setTimeout(() => {
+    $_(`#eject_${num}`).style.color = "rgba(0, 0, 0, 0.5)";
+    $_(`#eject_${num}`).style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    $_(`#eject_${num}`).innerHTML = "Eject ⏏";
+  }, 2000);
+  return;
 };
 
-const submit = (postData) => {
-  $("input[type='submit']").val("Saving");
-  $.ajax({
-    url: "savesettings/",
-    type: "POST",
-    data: postData,
-    success: () => {
-      $("input[type='submit']").val("Saved");
-      $("input[type='submit']").css("background-color", "#087f23");
-      $("#settings-form").trigger("reset");
-      $("#name").attr("placeholder", postData[0].value);
-      welcome(postData[0].value);
-      setTimeout(() => {
-        $("input[type='submit']").val("Save");
-        $("input[type='submit']").css("background-color", "#c79100");
-      }, 2000);
-    },
-    error: () => {
-      $("input[type='submit']").val("Error");
-      $("input[type='submit']").css("background-color", "#ba000d");
-      $("#settings-form").trigger("reset");
-      setTimeout(() => {
-        $("input[type='submit']").val("Save");
-        $("input[type='submit']").css("background-color", "#c79100");
-      }, 2000);
-    },
-  });
+// Save settings request
+const saveSettings = (settingsValues) => {
+  $_("#submit").innerHTML = "Saving";
+  const request = new XMLHttpRequest();
+  request.open("post", "savesettings/");
+  request.send(settingsValues);
+  request.onreadystatechange = () => {
+    if (request.readyState == 4) {
+      if (request.status == 200) saveSettingsSuccess(settingsValues.get("name"));
+      else saveSettingsError();
+    }
+  };
 };
+const saveSettingsSuccess = (name) => {
+  $_("#submit").value = "Saved";
+  $_("#submit").style.backgroundColor = "#087f23";
+  $_("#settings-form").reset();
+  $_("#name").setAttribute("placeholder", name);
+  welcome(name);
+  setTimeout(() => {
+    $_("#submit").value = "Save";
+    $_("#submit").style.backgroundColor = "#c79100";
+  }, 2000);
+};
+const saveSettingsError = () => {
+  $_("#submit").value = "Error";
+  $_("#submit").style.backgroundColor = "#ba000d";
+  $_("#settings-form").reset();
+  setTimeout(() => {
+    $_("#submit").value = "Save";
+    $_("#submit").style.backgroundColor = "#c79100";
+  }, 2000);
+};
+
+// Settings form
+$_("#settings-form").addEventListener("submit", (e) => {
+  let settingsValues = new FormData($_("#settings-form"));
+  if (
+    (settingsValues.get("interface") == "<%= iface %>" || !settingsValues.get("interface")) &&
+    !settingsValues.get("hostname") &&
+    !settingsValues.get("name") &&
+    !settingsValues.get("password")
+  ) {
+    e.preventDefault();
+    return;
+  } else if (settingsValues.get("interface") || settingsValues.get("hostname")) {
+    const rebootCofirm = window.confirm(
+      "One or more of the settings you changed will cause the device to reboot. Continue?",
+    );
+    if (rebootCofirm) {
+      saveSettings(settingsValues);
+      setTimeout(() => {
+        location.reload();
+      }, 2000);
+    } else {
+      $_("#settings-form").reset();
+    }
+  } else {
+    saveSettings(settingsValues);
+  }
+  e.preventDefault();
+});
--- Dashboard/server.js
+++ Dashboard/server.js
@@ -5,61 +5,71 @@ const app = express();
 const shell = require("shelljs");
 const multer = require("multer");
 const upload = multer();
-
 const infofile = require("./info.json");
-let name = infofile.name;
 
+//Properties
+let name = infofile.name;
 let hostname = shell.exec("hostname", { silent: true }).stdout.replace(/[\r\n]/g, "");
 let iface = shell.exec("route | grep '^default' | grep -o '[^ ]*$'", { silent: true }).stdout.replace(/[\r\n]/g, "");
-
 const properties = { name, hostname, iface };
 
+//Global variables
 let statuses = [];
 let devices;
 
+//Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(upload.array());
-
 app.use(express.static(path.join(__dirname + "/public")));
 
 app.engine("html", require("ejs").renderFile);
 
+//Homepage
 app.get("/", (req, res) => {
-  devices = JSON.parse(
-    shell.exec("sudo docker exec -u www-data nextcloud php /var/www/html/occ files_external:list --output json", {
+  command = shell.exec(
+    "sudo docker exec -u www-data nextcloud php /var/www/html/occ files_external:list --output json",
+    {
       silent: true,
-    }),
+    },
   );
+
+  if (command.code == 0) {
+    devices = JSON.parse(command);
+  } else {
+    devices = {};
+  }
   res.render(path.join(__dirname + "/index.html"), { name, hostname, iface, devices });
 });
 
-app.post("/adapter", () => {
+app.get("/adapter", () => {
+  console.log("Changing adapter");
   shell.exec("changedongle");
 });
-
-app.post("/reboot", () => {
-  shell.exec("sudo reboot");
+app.get("/reboot", () => {
+  reboot();
 });
-
-app.post("/shutdown", () => {
+app.get("/shutdown", () => {
+  console.log("Shutting down...");
   shell.exec("sudo shutdown now");
 });
 
+// Eject device
 app.post("/eject", (req, res) => {
   let index = parseInt(req.body.index);
   let mountid = devices[index].mount_id;
   let datadir = devices[index].configuration.datadir;
-  let command = shell.exec(`/etc/usbmount/umount.d/unmount ${mountid} ${datadir}`);
-  console.log(command.code);
+  let command = shell.exec(`/etc/usbmount/umount.d/unmount ${mountid} ${datadir}`, { silent: true });
   if (command.code === 0) {
+    console.log("Successful unmount");
     res.sendStatus(200);
   } else {
-    res.sendStatus(200);
+    console.log("Failed to unmount");
+    res.sendStatus(500);
   }
 });
-//Save settings
 
+//Save settings
 app.post("/savesettings", (req, res) => {
   req = req.body;
   if (req.name !== properties.name && req.name) namechange(req.name);
@@ -67,8 +77,10 @@ app.post("/savesettings", (req, res) => {
   if (req.password !== properties.password && req.password) passwordchange(req.password);
   if (req.iface !== properties.iface && req.iface) ifacechange(req.iface);
   if (statuses.includes(false)) {
+    console.log("Failed to save settings");
     res.sendStatus(500);
   } else {
+    console.log("Successfully saved settings");
     res.sendStatus(200);
     if ((req.iface !== properties.iface && req.iface) || (req.hostname !== properties.hostname && req.hostname)) {
       reboot();
@@ -90,7 +102,6 @@ const namechange = (newname) => {
     }
   });
 };
-
 const hostnamechange = (hostname) => {
   console.log(`Changing hostname to ${hostname}`);
   if (
@@ -107,11 +118,10 @@ const hostnamechange = (hostname) => {
       statuses.push(false);
     }
   } else {
-    console.log("Falied to change hostname");
+    console.log("Hostname uses invalid syntax");
     statuses.push(false);
   }
 };
-
 const passwordchange = (password) => {
   console.log(`Changing password to ${password}`);
   let command = shell.exec(`changepassword "${password}"`, { silent: true });
@@ -123,7 +133,6 @@ const passwordchange = (password) => {
     statuses.push(false);
   }
 };
-
 const ifacechange = (interface) => {
   console.log(`Changing interface to ${interface}`);
   let command = shell.exec(`changeinterface ${interface}`, { silent: true });
@@ -136,11 +145,13 @@ const ifacechange = (interface) => {
   }
 };
 
+//Reboot device
 const reboot = () => {
   console.log("Rebooting...");
   shell.exec("sudo reboot", { silent: true });
 };
 
+// Start server
 const port = 1234;
 app.listen(port);
 console.log(`Running at Port ${port}`);
